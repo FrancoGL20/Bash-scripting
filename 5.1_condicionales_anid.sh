58b282b8215cd8dda86de3953c46abe5b7adeaa2
FranciscoGL <francgutierrezlopez@gmail.com>
2024-03-19 20:36:07 -0600

    condicionales anidados

---
+++ 5.1_condicionales_anid.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+# Condicionales anidados
+
+read -p "Ingrese la primera nota (1-10): " nota1
+read -p "Ingrese la segunda nota (1-10): " nota2
+read -p "Ingrese la tercera nota (1-10): " nota3
+promedio=$((($nota1 + $nota2 + $nota3) / 3))
+
+echo "============================================="
+
+echo "el promedio del estudiante es: $promedio"
+
+if [ $promedio -ge 7 ]; then
+    echo "Estudiante aprobado"
+fi
